ef37a4fe8f2e887f69635909f61c6f73cb2d711f
Arnab <arnab.dutta@gmail.com>
2014-07-01 11:11:11 +0200

    adding mln formulation

---
+++ src/main/java/code/dws/reverb/mln/MLNCreator.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package code.dws.reverb.mln;
+
+/**
+ * @author arnab
+ */
+public class MLNCreator
+{
+
+    private static final String WEIGHTED_ASSERTION_FILE = null;
+
+    /**
+     * 
+     */
+    public MLNCreator()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        readTheWeightedFile(WEIGHTED_ASSERTION_FILE);
+    }
+
+    private static void readTheWeightedFile(String weightedAssertionFile)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
