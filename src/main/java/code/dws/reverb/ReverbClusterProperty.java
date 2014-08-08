81d678ff9e0d6751c4ce3167b4225883d76df20d
kraktos <arnab.dutta@gmail.com>
2014-08-08 20:02:58 +0200

    adding mods

--- src/main/java/code/dws/reverb/ReverbClusterProperty.java
+++ src/main/java/code/dws/reverb/ReverbClusterProperty.java
@@ -34,7 +34,7 @@ public class ReverbClusterProperty {
 	/**
 	 * top K most frequent Reverb properties
 	 */
-	public static int TOPK_REV_PROPS = 10;
+	public static int TOPK_REV_PROPS = 500;
 
 	private static final String DELIMIT = "\t";
 
@@ -43,7 +43,7 @@ public class ReverbClusterProperty {
 	 */
 	private static final String CLUSTERS = "CLUSTERS_TYPE";
 
-	private static final String CLUSTERS_NAME = "CLUSTERS_";
+	private static final String CLUSTERS_NAME = "src/main/resources/input/CLUSTERS_";
 
 	private static List<String> revbProps = null;
 
@@ -71,14 +71,6 @@ public class ReverbClusterProperty {
 		// enable scoring mechanism
 		doScoring(revbProps);
 
-		// getDistinctClusterNames(new String[][] {new String[]
-		// {"is a member of"}, new String[]
-		// {"is an active member of"}, new String[] {"is the spouse of"}, new
-		// String[] {"located in"}, new String[]
-		// {"will take place in"}});
-
-		// readProcessedFile(PREPROCESSED_REVERBFILE);
-
 		// dumpPropCluster();
 
 	}
@@ -94,6 +86,7 @@ public class ReverbClusterProperty {
 				CLUSTERS_NAME + "OVERLAP_" + TOPK_REV_PROPS));
 
 		// init DB
+
 		DBWrapper.init(Constants.GET_MOST_FREQUENT_SENSE);
 
 		try {
@@ -111,13 +104,14 @@ public class ReverbClusterProperty {
 							writerOverlap);
 
 					cnt++;
+					System.out.println("Completed " + (double) 200 * cnt
+							/ (properties.size() * (properties.size() - 1))
+							+ " %");
+
 				}
-
-				System.out.println("Completed " + (double) 200 * cnt
-						/ (properties.size() * (properties.size() - 1)) + " %");
-
 				writerOverlap.flush();
 				writerWordNet.flush();
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -210,151 +204,6 @@ public class ReverbClusterProperty {
 		writerWordNet
 				.write("sameAsPropWNConf(\"" + propArg1 + "\", \"" + propArg2
 						+ "\", " + Constants.formatter.format(score) + ")\n");
-
-	}
-
-	/**
-	 * method to get the baseline cluster without any MLN or intelligent ways
-	 * 
-	 * @param strings
-	 * @throws IOException
-	 */
-	private static void getDistinctClusterNames(String[][] strings)
-			throws IOException {
-		String[] reverbProperties = null;
-
-		if (strings.length == 0) {
-
-			BufferedWriter outputWriter = new BufferedWriter(new FileWriter(
-					CLUSTERS));
-
-			// init DB
-			DBWrapper.init(Constants.GET_DISTINCT_REVERB_PROP_CLUSTERS);
-
-			try {
-				List<String> results = DBWrapper
-						.fetchDistinctReverbClusterNames();
-
-				// iterate the distinct cluster names = [Domain, Range]
-				for (String val : results) {
-
-					outputWriter.write(val.split("\t")[0] + "\t"
-							+ val.split("\t")[1] + "\n");
-
-					reverbProperties = val.split("\t")[2].split("~");
-
-					for (String prop : reverbProperties) {
-						outputWriter.write("\t" + prop + "\t"
-								+ WordNetAPI.getSynonyms(prop) + "\n");
-
-						// look for wordnet senses
-						// System.out.println(prop + "==> " +
-						// WordNetAPI.getSynonyms(prop));
-
-					}
-
-					outputWriter.flush();
-				}
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				outputWriter.close();
-				DBWrapper.shutDown();
-			}
-		} else {
-
-			// compute the intra property sim score using Wordnet
-			for (int outer = 0; outer < strings.length - 1; outer++) {
-				for (int inner = outer + 1; inner < strings.length; inner++) {
-					System.out.println("COMPARING:  "
-							+ strings[outer][0].toString()
-							+ " \t "
-							+ strings[inner][0].toString()
-							+ "\t"
-							+ WordNetAPI.scoreWordNet(strings[outer],
-									strings[inner]));
-				}
-			}
-		}
-	}
-
-	/**
-	 * read the pre processed Reverb triples and cluster them on domain, range
-	 * 
-	 * @param filePath
-	 */
-	private static void readProcessedFile(String filePath) {
-		Scanner scan;
-		String sCurrentLine;
-		String[] strArr = null;
-		double prob = 0;
-		double weight = 0;
-
-		String domain = null;
-		String range = null;
-		String property = null;
-
-		scan = new Scanner(
-				ReverbClusterProperty.class.getResourceAsStream(filePath),
-				"UTF-8");
-
-		Pair<String, String> pairConcepts = null;
-
-		while (scan.hasNextLine()) {
-			sCurrentLine = scan.nextLine();
-
-			strArr = sCurrentLine.split(DELIMIT);
-			prob = Double.parseDouble(strArr[0]);
-			weight = Double.parseDouble(strArr[1]);
-
-			domain = strArr[2];
-			property = strArr[4];
-			range = strArr[6];
-
-			pairConcepts = new ImmutablePair<String, String>(domain.trim(),
-					range.trim());
-
-			// put all the weighted types for each properties in memory
-			updateMap(pairConcepts, property, weight);
-
-			if (MAP_CLUSTER.size() % 1000 == 0)
-				System.out.println("Processed " + MAP_CLUSTER.size());
-		}
-
-	}
-
-	/**
-	 * put the romain range pair as key and the associated proeprty value with
-	 * weights in a memory collection
-	 * 
-	 * @param pair
-	 * @param property
-	 * @param weight
-	 */
-	private static void updateMap(Pair<String, String> pair, String property,
-			double weight) {
-
-		Map<String, Double> values = null;
-
-		if (MAP_CLUSTER.containsKey(pair)) {
-			// get old collection
-			values = MAP_CLUSTER.get(pair);
-		} else {
-			// create new collection
-			values = new HashMap<String, Double>();
-		}
-
-		if (!values.containsKey(property)) {
-			values.put(property, weight);
-		} else {
-			if (values.get(property) < weight) {
-				// update the property with a better score
-				values.put(property, weight);
-			}
-		}
-
-		MAP_CLUSTER.put(pair, values);
 
 	}
 
